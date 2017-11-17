f52a4f5f4b2fb0df425c2039cd9145de79d11e76
AndyCui <13520061695@163.com>
2017-11-17 15:30:18 +0800

    整个项目

---
+++ src/main/java/person/andy/EurekaServerCopyApplication.java
@@ -0,0 +1,14 @@
+package person.andy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerCopyApplication.class, args);
+	}
+}
